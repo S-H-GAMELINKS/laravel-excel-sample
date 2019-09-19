9b9c5a3d58852bdb223eced7972847fa012811b4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-19 17:26:15 +0900

    add import action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,4 +32,11 @@ class HomeController extends Controller
     {
         return Excel::download(new UsersExport, 'users.xlsx');
     }
+
+    public function import(Request $request)
+    {
+        Excel::import(new UsersImport, request()->file('file'));
+
+        return redirect('/')->with('success', 'All good!');
+    }
 }
