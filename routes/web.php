3ce9cfd77f3519b4fdbc5d632a26cfd063daaf70
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-19 17:26:46 +0900

    add /users/import route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('users/export/', 'HomeController@export');
+
+Route::get('users/import/', 'HomeController@import');
